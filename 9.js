597aa7e4c352f0510ea7fc28825574cb5965289a
Rohil-Solutelabs <125549822+Rohil-Solutelabs@users.noreply.github.com>
2023-02-16 20:04:59 +0530

    Update 9.js

--- 9.js
+++ 9.js
@@ -4,7 +4,7 @@ const dup_arr = (arr) => {
   let sortedArray = arr.sort();
   let result = [];
 
-  for (let i = 0; i < sortedArray.length - 1; i++) {
+  for (let i = 0; i < sortedArray.length; i++) {
     if (sortedArray[i + 1] === sortedArray[i]) {
       // if index 1 and index 0 is same then we have to push this element in result array
       result.push(sortedArray[i]);
