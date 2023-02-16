314cc6f97a3a7e7a4fdc08b13de55209d714277a
Rohil-Solutelabs <125549822+Rohil-Solutelabs@users.noreply.github.com>
2023-02-16 17:03:46 +0530

    Add files via upload

---
+++ 1.2.js
@@ -0,0 +1,22 @@
+// using object -------------------
+
+const arr = ["hello", "world", "java", "hello", "java"];
+
+// First I create empty object name = frequency
+const frequency = {};
+
+for (let freq = 0; freq < arr.length; freq++) {
+  let element = arr[freq];
+
+  // update frequency count of an element in frequency object
+  if (frequency[element]) {
+    frequency[element] = frequency[element] + 1;
+  } else {
+    frequency[element] = 1;
+  }
+}
+
+console.log(frequency);
+
+
+
