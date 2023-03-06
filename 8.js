73ad344633d22811abad7379c59a6dca8af214a9
Rohil <125549822+Rohil-Solutelabs@users.noreply.github.com>
2023-03-06 13:43:36 +0530

    Update 8.js

--- 8.js
+++ 8.js
@@ -4,16 +4,14 @@ const arr = [1, 3, 5, 1, 4, 8, 2, 5, 6, 8, 8, 3, 2, 7];
 
 const dup_arr = (arr) => {
   let sortedArray = arr.sort();
-  console.log("Sorted Array", sortedArray);
 
   let result = [];
 
   for (let i = 0; i < sortedArray.length; i++) {
     if (
-      sortedArray[i + 1] == sortedArray[i] ||
-      sortedArray[i - 1] == sortedArray[i]
+      sortedArray[i + 1] !== sortedArray[i] &&
+      sortedArray[i - 1] !== sortedArray[i]
     ) {
-    } else {
       result.push(sortedArray[i]);
     }
   }
