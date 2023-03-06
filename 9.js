66d50df13aae7fc2f7c96ca4e950ac9da5155f87
Rohil <125549822+Rohil-Solutelabs@users.noreply.github.com>
2023-03-06 17:20:38 +0530

    Update 9.js

--- 9.js
+++ 9.js
@@ -1,20 +1,23 @@
+// Print all duplicate elements of an array
+
 const arr = [1, 3, 5, 1, 4, 8, 2, 5, 6, 8, 8, 3, 2, 7];
 
 const dup_arr = (arr) => {
-  let sortedArray = arr.sort();
+  let map = {};
   let result = [];
-
-  for (let i = 0; i < sortedArray.length; i++) {
-    if (
-      sortedArray[i + 1] === sortedArray[i] ||
-      sortedArray[i - 1] === sortedArray[i]
-    ) {
-      // if index 1 and index 0 is same then we have to push this element in result array
-      result.push(sortedArray[i]);
+  for (let i = 0; i < arr.length; i++) {
+    if (!map[arr[i]]) {
+      map[arr[i]] = 1;
+    } else {
+      map[arr[i]]++;
     }
   }
-
-  let set = new Set(result);
-  console.log(set);
+  for (let key in map) {
+    if (map[key] > 1) {
+      result.push(key);
+    }
+  }
+  console.log(result);
 };
+
 dup_arr(arr);
